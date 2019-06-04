94982111607ea90eebd652ac420965337468f37f
unknown <carlos.agustin.diaz1998@gmail.com>
2019-06-04 20:22:02 -0300

    Props

--- hello-world/src/components/Card.js
+++ hello-world/src/components/Card.js
@@ -3,20 +3,41 @@
 import React from 'react'
 // importamos la imagen / css
 import exerciseImg from '../imagens/exercise.png'
+import circleImg from '../imagens/circles.png'
 import './Style/Card.css'
 class Card extends React.Component{
+    
+    // constructor(props){
+    //     super(props)
+    //     this.state = { 
+    //         image: ''
+    //     }
+    // }
+
+    // // ciclo de vida
+    // componentDidMount(){
+    //     setTimeout(() => {
+    //         this.setState({
+    //           image:''  
+    //         })
+    //     }, 5000)
+    // }
     // necesitamos definir Render
     render(){
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card"
+                style={{
+                    backgroundImage: `url(${circleImg}), linear-gradient(to right,${this.props.leftColor} , ${this.props.rightColor})`
+                }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exerciseImg} className="float-right" />
+                            <img src={this.props.img} className="float-right" />
                         </div>
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calisthenics</p>
+                            <h1>{this.props.title}</h1>
+                            <p>{this.props.description}</p>
                         </div>
                     </div>
                 </div>
--- hello-world/src/index.js
+++ hello-world/src/index.js
@@ -12,6 +12,12 @@ import Card from './components/Card'
 const container = document.getElementById('root')
 
 // ReactDOM.render() recibe 2 parametros,  que y donde renderizar
-ReactDOM.render(<Card />,container) 
+ReactDOM.render(<Card 
+                     title="Technique Guides"
+                     description="Learn amazing street workout and calisthenics"
+                     img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                     leftColor="#A74CF2"
+                     rightColor="#617BFB"
+                />,container) 
 
 // se pasa el componente como una etiqueta
