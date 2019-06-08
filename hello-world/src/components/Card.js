77858660a19efadb6a1aa4d588f3020d11486558
unknown <carlos.agustin.diaz1998@gmail.com>
2019-06-08 15:55:53 -0300

    clase 29 -  react Frac

--- hello-world/src/components/Card.js
+++ hello-world/src/components/Card.js
@@ -1,42 +1,26 @@
-// crear component
-
 import React from 'react'
-// importamos la imagen / css
-import circleImg from '../imagens/circles.png'
+import circlesImg from '../imagens/circles.png'
+import emptyImg from '../imagens/empty.png'
 import './Style/Card.css'
-class Card extends React.Component{
-    
-    // constructor(props){
-    //     super(props)
-    //     this.state = { 
-    //         image: ''
-    //     }
-    // }
 
-    // // ciclo de vida
-    // componentDidMount(){
-    //     setTimeout(() => {
-    //         this.setState({
-    //           image:''  
-    //         })
-    //     }, 5000)
-    // }
-    // necesitamos definir Render
+class Card extends React.Component {
+
     render(){
+        const { title, description, img, leftColor, rightColor } = this.props
         return (
             <div className="card mx-auto Fitness-Card m-3"
-                style={{
-                    backgroundImage: `url(${circleImg}), linear-gradient(to right,${this.props.leftColor} , ${this.props.rightColor})`
-                }}
+            style={{
+                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor || '#56CCF2'}  , ${rightColor|| '#2F80ED'}) `
+            }}
             >
                 <div className="card-body">
                     <div className="row center">
-                        <div className="col-6 cont-img">
-                            <img src={this.props.img} className="float-right" alt="exercise"/>
-                        </div>
+                        <div className="col-6">
+                            <img src={img || emptyImg} className="float-right" alt="exercise"/>
+                        </div> 
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>{this.props.title}</h1>
-                            <p>{this.props.description}</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
@@ -44,4 +28,5 @@ class Card extends React.Component{
         )
     }
 }
+
 export default Card
